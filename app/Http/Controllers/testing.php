239732bc85dba407adff67ef604fbc5cd7d46ce8
Dynath25 <dodyprama25@gmail.com>
2023-12-10 12:26:32 +0800

    initial commit

---
+++ app/Http/Controllers/testing.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class testing extends Controller
+{
+    public function kitacoba()
+    {
+        return Response(["saya mencoba belajar Json API","halooooooo"]) ;
+    }
+}
